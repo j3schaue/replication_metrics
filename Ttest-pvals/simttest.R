0b9ef2f65ccad01f32f9a21d8e04db1cbfd22485
menawhalen <mcwhalen@u.northwestern.edu>
2018-04-12 12:03:09 -0500

    Simulations of some cases of pvalues on the t-test

--- Ttest-pvals/simttest.R
+++ Ttest-pvals/simttest.R
@@ -1,15 +1,15 @@
 #setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 source("./src/metrics_funs.R")
 ### Case 1 different thetas but same power
-N<- 1000
+N<- 1000 
 tmatrix<-matrix(data = NA, nrow = N, ncol = 3)
 for (k in 1:N){
   
-  t1<-rep(.2, 100)
+  t1<-rep(.2, 100) # Small theta values
   sim_1_small<-simulate_studies(t1, 4/393) # 393 sample size needed for 80% power is small 
-  t2<-rep(.5, 100)
+  t2<-rep(.5, 100) # medium theta values
   sim_1_med<-simulate_studies(t2, 4/64) # 64 is samples size needed for 80% power if medium
-  t3<-rep(.8,100)
+  t3<-rep(.8,100) # large theta values
   sim_1_large<-simulate_studies(t3, 4/25) # 25 is samples size needed for 80% power if medium
   
   #difference in each pvalue
@@ -18,6 +18,7 @@ for (k in 1:N){
   diff_medlarge<-sim_1_med$p - sim_1_large$p 
   
   # find t statistics
+  # single t statistics for given disparity
   tsmallmed<- mean(diff_smallmed)/(sd(diff_smallmed)/10)
   tsmalllarge<- mean(diff_smalllarge)/(sd(diff_smalllarge)/10)
   tmedlarge<- mean(diff_medlarge)/(sd(diff_medlarge)/10)
@@ -27,11 +28,11 @@ for (k in 1:N){
   tmatrix[k,3]<-tmedlarge
   
 }
-case1_smallmed<-tmatrix[,1]
-case1_smalllarge<-tmatrix[,2]
-case1_medlarge<-tmatrix[,3]
-sum(tmatrix[,1] > 1.98)/N
-sum(tmatrix[,2] > 1.98)/N
-sum(tmatrix[,3] > 1.98)/N
 
+sum(abs(tmatrix[,1]) > 1.98)/N # prob of test concluding that they are different for small and meduim differences
+sum(abs(tmatrix[,2]) > 1.98)/N # prob of test concluding that they are different for small and large differences
+sum(abs(tmatrix[,3]) > 1.98)/N # prob of test concluding that they are different for medium and large differences
+
+
+## Case 2 same thetas with different powers
   
