7de227b49fdaede7a1ff26d04759ee3b69f2532d
menawhalen <mcwhalen@u.northwestern.edu>
2018-04-14 15:26:01 -0500

    Simulation of p-values with t test

--- Ttest-pvals/simttest.R
+++ Ttest-pvals/simttest.R
@@ -1,16 +1,16 @@
-#setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+.llllllllllllllllllllllllllllllllll..#setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 source("./src/metrics_funs.R")
 ### Case 1 different thetas but same power
-N<- 1000 
+N<- 100000 
 tmatrix<-matrix(data = NA, nrow = N, ncol = 3)
 for (k in 1:N){
   
   t1<-rep(.2, 100) # Small theta values
-  sim_1_small<-simulate_studies(t1, 4/393) # 393 sample size needed for 80% power is small 
+  sim_1_small<-simulate_studies(t1, 4/(2*393)) # 393 sample size needed for 80% power is small 
   t2<-rep(.5, 100) # medium theta values
-  sim_1_med<-simulate_studies(t2, 4/64) # 64 is samples size needed for 80% power if medium
+  sim_1_med<-simulate_studies(t2, 4/(2*64)) # 64 is samples size needed for 80% power if medium
   t3<-rep(.8,100) # large theta values
-  sim_1_large<-simulate_studies(t3, 4/25) # 25 is samples size needed for 80% power if medium
+  sim_1_large<-simulate_studies(t3, 4/(2*25)) # 25 is samples size needed for 80% power if medium
   
   #difference in each pvalue
   diff_smallmed<-sim_1_small$p - sim_1_med$p 
