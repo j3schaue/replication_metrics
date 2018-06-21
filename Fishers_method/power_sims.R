51ecc58e1a7bc4ec8622545a60170a514a22da77
Katie Fitzgerald <Katie@dhcp-10-105-108-107.wireless.northwestern.private>
2018-06-20 19:46:52 -0500

    Edits to shortened write-up, cleaned power_sims code

--- Fishers_method/power_sims.R
+++ Fishers_method/power_sims.R
@@ -7,26 +7,41 @@ power_sims<-function(N,M,delta,n){
   # RETURNS: power of Fisher's test to reject
   # Assumes 2-sided p-values, throws away p-values<=0.05 to match OSC methods
   #########################################################################################
-  T<-c() #empty list for Fisher's test statistic 
+  
+  T<-c() #empty list to store Fisher's test statistic 
   Power<-matrix() #empty matrix to store results
+  
   for(k in 1:length(M)){
+    
     for (i in 1:N){
-      print(i) #uncomment to show progress for lengthy simulations
-      p0<-runif(64-M[k],0.05,1) #draws p-values for the true negatives
-      p1<-c() 
-      for (j in 1:M[k]){
+      
+      #print(i) # can uncomment to show progress for lengthy simulations
+    
+      p0<-runif(64 - M[k], 0.05, 1) #draws p-values for the true null effects
+      
+      p1<-c() #create list to store p-values drawn for non-null effects
+      
+      for (j in 1:M[k]){ 
+        
         p1[j]<-0
-        while (p1[j]<=0.05) { #throw away p-values<=0.05
-          p1[j]<-2*(1-pnorm(abs(rnorm(1,delta,sqrt(2/n[j])))/sqrt(2/n[j])))
+        
+        while (p1[j] <= 0.05) { #throw away p-values<=0.05
+          p1[j]<-2*(1 - pnorm(abs(rnorm(1, delta, sqrt(2/n[j])))/sqrt(2/n[j])))
         }
-        print(n[j]) #uncomment to show progress for lengthy simulations 
-      }
+        
+        #print(n[j]) # can uncomment to show progress for lengthy simulations 
+        }
+      
       #test statistic for Fisher's method, with transformation for truncating p-values
       T[i]<--2*sum(log((p0-0.05)/0.95))-2*sum(log((p1-0.05)/0.95)) 
-    }
+    
+      }
+    
     Power[k]<-sum(T>155.4047)/N
-  }
-  return(Power)
+  
+    }
+  
+    return(Power)
 }
 
 M<-c(1,2,3,4,5,10,32,64) #vector of number of non-null effects
