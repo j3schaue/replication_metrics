dd732e1911cb95c3fdbd7eb219fa48c45ae53abb
Katie Fitzgerald <Katie@dhcp-10-105-180-122.wireless.northwestern.private>
2018-07-25 15:06:08 -0500

    reformatting data for plot, create plot

--- Fishers_method/power_sims.R
+++ Fishers_method/power_sims.R
@@ -127,10 +127,137 @@ saveRDS(table_k100_n25,"table_k100_n25.RDS")
 #n=50
 n<-rep(50,100)
 power_delta_0.2_k100_n50 <-power_sims(100000,M,0.2,n,100)
+saveRDS(power_delta_0.2_k100_n50,"power_delta_0.2_k100_n50.RDS")
 power_delta_0.5_k100_n50<-power_sims(100000,M,0.5,n,100)
+saveRDS(power_delta_0.5_k100_n50,"power_delta_0.5_k100_n50.RDS")
 power_delta_0.8_k100_n50<-power_sims(100000,M,0.8,n,100)
-table_k100_n25<-data.frame(M,power_delta_0.2_k100_n25,power_delta_0.5_k100_n25,power_delta_0.8_k100_n25)
-saveRDS(power_delta_0.2_k100_n25,"power_delta_0.2_k100_n25.RDS")
-saveRDS(power_delta_0.5_k100_n25,"power_delta_0.5_k100_n25.RDS")
-saveRDS(power_delta_0.8_k100_n25,"power_delta_0.8_k100_n25.RDS")
-saveRDS(table_k100_n25,"table_k100_n25.RDS")
+saveRDS(power_delta_0.8_k100_n50,"power_delta_0.8_k100_n50.RDS")
+table_k100_n50<-data.frame(M,power_delta_0.2_k100_n50,power_delta_0.5_k100_n50,power_delta_0.8_k100_n50)
+saveRDS(table_k100_n50,"table_k100_n50.RDS")
+
+#n=75
+n<-rep(75,100)
+power_delta_0.2_k100_n75 <-power_sims(100000,M,0.2,n,100)
+saveRDS(power_delta_0.2_k100_n75,"power_delta_0.2_k100_n75.RDS")
+power_delta_0.5_k100_n75<-power_sims(100000,M,0.5,n,100)
+saveRDS(power_delta_0.5_k100_n75,"power_delta_0.5_k100_n75.RDS")
+power_delta_0.8_k100_n75<-power_sims(100000,M,0.8,n,100)
+saveRDS(power_delta_0.8_k100_n75,"power_delta_0.8_k100_n75.RDS")
+table_k100_n75<-data.frame(M,power_delta_0.2_k100_n75,power_delta_0.5_k100_n75,power_delta_0.8_k100_n75)
+saveRDS(table_k100_n75,"table_k100_n75.RDS")
+
+#n=100
+n<-rep(100,100)
+power_delta_0.2_k100_n100 <-power_sims(100000,M,0.2,n,100)
+saveRDS(power_delta_0.2_k100_n100,"power_delta_0.2_k100_n100.RDS")
+power_delta_0.5_k100_n100<-power_sims(100000,M,0.5,n,100)
+saveRDS(power_delta_0.5_k100_n100,"power_delta_0.5_k100_n100.RDS")
+power_delta_0.8_k100_n100<-power_sims(100000,M,0.8,n,100)
+saveRDS(power_delta_0.8_k100_n100,"power_delta_0.8_k100_n100.RDS")
+table_k100_n100<-data.frame(M,power_delta_0.2_k100_n100,power_delta_0.5_k100_n100,power_delta_0.8_k100_n100)
+saveRDS(table_k100_n100,"table_k100_n100.RDS")
+
+##get data in correct format
+k10_n25_d.2 <- as.data.frame(table_k10_n25[,2])
+colnames(k10_n25_d.2)[1] <- "power"
+k10_n25_d.2$n <- rep(25,length(k10_n25_d.2))
+k10_n25_d.2$k <- rep(10,length(k10_n25_d.2))
+k10_n25_d.2$M <- c(1,2,3,4,5,10)
+k10_n25_d.2$delta <- rep(0.2, dim(k10_n25_d.2)[1])
+
+k10_n25_d.5 <- as.data.frame(table_k10_n25[,3])
+colnames(k10_n25_d.5)[1] <- "power"
+k10_n25_d.5$n <- rep(25,length(k10_n25_d.5))
+k10_n25_d.5$k <- rep(10,length(k10_n25_d.5))
+k10_n25_d.5$M <- c(1,2,3,4,5,10)
+k10_n25_d.5$delta <- rep(0.5, dim(k10_n25_d.5)[1])
+
+k10_n25_d.8 <- as.data.frame(table_k10_n25[,4])
+colnames(k10_n25_d.8)[1] <- "power"
+k10_n25_d.8$n <- rep(25,length(k10_n25_d.8))
+k10_n25_d.8$k <- rep(10,length(k10_n25_d.8))
+k10_n25_d.8$M <- c(1,2,3,4,5,10)
+k10_n25_d.8$delta <- rep(0.8, dim(k10_n25_d.8)[1])
+
+
+##########
+k10_n50_d.2 <- as.data.frame(table_k10_n50[,2])
+colnames(k10_n50_d.2)[1] <- "power"
+k10_n50_d.2$n <- rep(50,length(k10_n50_d.2))
+k10_n50_d.2$k <- rep(10,length(k10_n50_d.2))
+k10_n50_d.2$M <- c(1,2,3,4,5,10)
+k10_n50_d.2$delta <- rep(0.2, dim(k10_n50_d.2)[1])
+
+k10_n50_d.5 <- as.data.frame(table_k10_n50[,3])
+colnames(k10_n50_d.5)[1] <- "power"
+k10_n50_d.5$n <- rep(50,length(k10_n50_d.5))
+k10_n50_d.5$k <- rep(10,length(k10_n50_d.5))
+k10_n50_d.5$M <- c(1,2,3,4,5,10)
+k10_n50_d.5$delta <- rep(0.5, dim(k10_n50_d.5)[1])
+
+k10_n50_d.8 <- as.data.frame(table_k10_n50[,4])
+colnames(k10_n50_d.8)[1] <- "power"
+k10_n50_d.8$n <- rep(50,length(k10_n50_d.8))
+k10_n50_d.8$k <- rep(10,length(k10_n50_d.8))
+k10_n50_d.8$M <- c(1,2,3,4,5,10)
+k10_n50_d.8$delta <- rep(0.8, dim(k10_n50_d.8)[1])
+
+########
+k10_n75_d.2 <- as.data.frame(table_k10_n75[,2])
+colnames(k10_n75_d.2)[1] <- "power"
+k10_n75_d.2$n <- rep(75,length(k10_n75_d.2))
+k10_n75_d.2$k <- rep(10,length(k10_n75_d.2))
+k10_n75_d.2$M <- c(1,2,3,4,5,10)
+k10_n75_d.2$delta <- rep(0.2, dim(k10_n75_d.2)[1])
+
+k10_n75_d.5 <- as.data.frame(table_k10_n75[,3])
+colnames(k10_n75_d.5)[1] <- "power"
+k10_n75_d.5$n <- rep(75,length(k10_n75_d.5))
+k10_n75_d.5$k <- rep(10,length(k10_n75_d.5))
+k10_n75_d.5$M <- c(1,2,3,4,5,10)
+k10_n75_d.5$delta <- rep(0.5, dim(k10_n75_d.5)[1])
+
+k10_n75_d.8 <- as.data.frame(table_k10_n75[,4])
+colnames(k10_n75_d.8)[1] <- "power"
+k10_n75_d.8$n <- rep(75,length(k10_n75_d.8))
+k10_n75_d.8$k <- rep(10,length(k10_n75_d.8))
+k10_n75_d.8$M <- c(1,2,3,4,5,10)
+k10_n75_d.8$delta <- rep(0.8, dim(k10_n75_d.8)[1])
+
+########
+k10_n100_d.2 <- as.data.frame(table_k10_n100[,2])
+colnames(k10_n100_d.2)[1] <- "power"
+k10_n100_d.2$n <- rep(100,length(k10_n100_d.2))
+k10_n100_d.2$k <- rep(10,length(k10_n100_d.2))
+k10_n100_d.2$M <- c(1,2,3,4,5,10)
+k10_n100_d.2$delta <- rep(0.2, dim(k10_n100_d.2)[1])
+
+k10_n100_d.5 <- as.data.frame(table_k10_n100[,3])
+colnames(k10_n100_d.5)[1] <- "power"
+k10_n100_d.5$n <- rep(100,length(k10_n100_d.5))
+k10_n100_d.5$k <- rep(10,length(k10_n100_d.5))
+k10_n100_d.5$M <- c(1,2,3,4,5,10)
+k10_n100_d.5$delta <- rep(0.5, dim(k10_n100_d.5)[1])
+
+k10_n100_d.8 <- as.data.frame(table_k10_n100[,4])
+colnames(k10_n100_d.8)[1] <- "power"
+k10_n100_d.8$n <- rep(100,length(k10_n100_d.8))
+k10_n100_d.8$k <- rep(10,length(k10_n100_d.8))
+k10_n100_d.8$M <- c(1,2,3,4,5,10)
+k10_n100_d.8$delta <- rep(0.8, dim(k10_n100_d.8)[1])
+
+k10 <- rbind(k10_n25_d.2,k10_n25_d.5,k10_n25_d.8,k10_n50_d.2,k10_n50_d.5,k10_n50_d.8,k10_n75_d.2, k10_n75_d.5, k10_n75_d.8,k10_n100_d.2,k10_n100_d.5,k10_n100_d.8)
+
+library(ggplot2)
+labels <- c('25' = "n=25", '50' = "n=50", '75' = "n=75", '100'= "n=100")
+ggplot(k10, aes(x = M, y = power, color = factor(delta))) +
+  theme(panel.grid.minor = element_blank(),
+        axis.ticks = element_blank(), plot.title = element_text(hjust = 0.5)) +
+  scale_x_continuous(name = "# of false negatives", limits = c(1,10), breaks = seq(1,10,1)) +
+  scale_y_continuous(name = "Power", limits = c(0,1), breaks = seq(0,1,0.1))+
+  geom_point(size = 3) +
+  facet_grid(.~n, labeller = labeller(n = labels)) +
+  guides(fill=guide_legend(title="New Legend Title"))+
+  labs(title = "Power of Fisher's method for k=10 studies", color = expression(delta))+
+  geom_smooth(method = "lm", se = FALSE, size = 0.5)
+
