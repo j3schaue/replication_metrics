7d6ba661e64d5603a8026f2c9d56385e81d316d4
Katie Fitzgerald <Katie@dhcp-10-105-134-41.wireless.northwestern.private>
2019-01-14 12:10:52 -0600

    rrita's simulation results (rpe + rpp_delta_0.2)

--- Create Table 1 for REP and RPP data.R
+++ Create Table 1 for REP and RPP data.R
@@ -4,3 +4,9 @@ subset_RPP_replicate <- full_data_rep_fisher[full_data_rep_fisher$data == "RPP" 
 subset$t <- subset$d / subset$vd
 subset$significance <- ifelse(abs(subset$t)>1.96, 1, 0)
 subset$n <- (8+subset$d^2)/(4*subset$vd)
+
+rpp_data_cleaned <- read_csv("Fishers_method/rpp_data_cleaned.csv")
+rpp$z.test <- rpp$d/sqrt(rpp$vd)
+rpp_fishers <- subset(rpp_data_cleaned, rpp_data_cleaned$replicate == 1 & rpp_data_cleaned$pvalr > 0.05)
+
+rpp_fishers$n_effective <- (8+(rpp_fishers$d)^2)/(4*rpp_fishers$vd)
