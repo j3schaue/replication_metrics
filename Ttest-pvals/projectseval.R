77663c1a51e4c5c843790a6b6c6b706b97d4ca2a
menawhalen <mcwhalen@u.northwestern.edu>
2019-01-16 15:30:35 -0600

    finished prject have both tables

--- Ttest-pvals/projectseval.R
+++ Ttest-pvals/projectseval.R
@@ -8,20 +8,60 @@ fulldat<-readRDS("C:/Users/Mena Whalen/Downloads/full_data_rep_fisher.RDS")
 #                         vd = variance of effect size estimate
 #                        replicated = 0 if authors determined if replication failed, 1 if succeeded
 
+# t test function for the same theta
+ttestsameFunc<-function(theta){
+  
+fulldat %>% 
+  mutate(theta = rep(theta, length(data))) %>%
+  mutate(pval = pval_fun(theta, vd) )%>%
+  group_by(data,experiment) %>%
+  spread(site, pval) %>%
+  mutate(orig = sum(original, na.rm=T), rep=sum(replicate, na.rm=T)) %>%
+  mutate(diff = orig - rep) %>%
+  distinct(data, experiment, orig, rep, diff) %>%
+  ungroup() %>%
+  group_by(data) %>%
+  summarise( n = length(data),
+             mean = mean(diff),
+             sd = sd(diff),
+             power = power.t.test(n=n, delta = mean(diff), sd = sd(diff), sig.level = 0.05, type = "paired", alternative = "two.sided")$power
+             )
+}
 
-ttestF<-function(theta){
+ttestsameFunc(0.2)
+ttestsameFunc(0.5)
+ttestsameFunc(0.8)
+
+
+
+####################################################################################
+# t test function for different thetas
+##bigger effects go into theta1
+
+
+ttestdiffFunc<-function(theta1, theta2){
+  fulldat %>% 
+    mutate(theta = ifelse(site == "original", theta1, theta2)) %>%
+    mutate(pval = pval_fun(theta, vd)) %>%
+    group_by(data,experiment) %>%
+    spread(site, pval) %>%
+    mutate(orig = sum(original, na.rm=T), rep=sum(replicate, na.rm=T)) %>%
+    mutate(diff = orig - rep) %>%
+    distinct(data, experiment, orig, rep, diff) %>%
+    ungroup() %>%
+    group_by(data) %>%
+    summarise( n = length(data),
+               mean = mean(diff),
+               sd = sd(diff),
+               power = 1-power.t.test(n=n, delta = mean(diff), sd = sd(diff), sig.level = 0.05, type = "paired", alternative = "two.sided")$power
+    )
   
   
 }
-theta<-0.2
 
-fulldat %>% 
-  mutate(theta = rep(theta, length(data))) %>%
-  mutate(pval = pval_fun(d, vd) )%>%
-  group_by(experiment, site) %>%
-  spread(site, pval) %>%
-  mutate(orig = sum(original, na.rm=T), rep=sum(replicate, na.rm=T)) %>%
-  mutate(tstat = ifelse(orig <0.00001 & rep < 0.00001, NA, mean(orig - rep)/sd(orig - rep))) %>% 
-  mutate(isSIG = abs(tstat) > 1.98)
-  
-  
+ttestdiffFunc(0.8,0)
+ttestdiffFunc(0.8,0.2)
+ttestdiffFunc(0.8,0.5)
+ttestdiffFunc(0.5,0)
+ttestdiffFunc(0.5,0.2)
+ttestdiffFunc(0.2,0)
